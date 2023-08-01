7c314d4f808f0d51714c86f737818264faa0388b
Meowmaster <ashenoy64@gmail.com>
2023-08-01 10:44:48 +0530

    Removed cache files

--- AI.py
+++ AI.py
@@ -1,3 +1,4 @@
+import display
 from vosk import Model, KaldiRecognizer
 import pyaudio
 import pyttsx3 as tts
@@ -8,11 +9,10 @@ import os
 from io import BytesIO
 import sys
 sys.path.append("./oled")
-import display
 model = Model("vosk-model-en-in-0.5")
 # wakeWordRecog=KaldiRecognizer(model,16000,'["mard","mard"]')
 recognizer = KaldiRecognizer(model, 16000)
-flag=True
+flag = True
 
 
 class Timer:
@@ -24,23 +24,22 @@ class Timer:
         self.engine.setProperty('volume', 0.9)
         self.engine.setProperty('voice', 'english_rp+f4')
 
-    def func(self,tsec):
+    def func(self, tsec):
         time.sleep(tsec)
         if self.engine._inLoop:
             self.engine.endLoop()
         self.engine.say("Time is Up")
         self.engine.runAndWait()
 
-
     def process(self, inputObj):
-        str_time=''
-        if len(inputObj)==1:
-            str_time=inputObj.values()[0]
+        str_time = ''
+        if len(inputObj) == 1:
+            str_time = inputObj.values()[0]
         else:
             try:
                 str_time = inputObj['TimeInSeconds']
             except:
-                str_time='5'
+                str_time = '5'
         tsec = int(str_time)
         thread = threading.Thread(target=self.func, args=(tsec,))
         thread.start()
@@ -68,31 +67,31 @@ class Task:
 
 
 class AI:
-    
-    def __init__(self, api_key_1=None, api_key_2=None, pluginArray=[]):
-        
-                if api_key_1 == None or api_key_2 == None:
-                    exit()
-                
-                self.oled=display.Display()
-                self.stopEvent=threading.Event()
-                self.startDisplay("start")
-                openai.api_key = api_key_1
-                self.api_key_1=api_key_1
-                self.api_key_2=api_key_2
 
-                self.aiInstructions = '''The user will provide a phrase, and  check if it is a command or not. If it is a supported command response format:{}.
+    def __init__(self, api_key_1=None, api_key_2=None, pluginArray=[]):
+
+        if api_key_1 == None or api_key_2 == None:
+            exit()
+
+        self.oled = display.Display()
+        self.stopEvent = threading.Event()
+        self.startDisplay("start")
+        openai.api_key = api_key_1
+        self.api_key_1 = api_key_1
+        self.api_key_2 = api_key_2
+
+        self.aiInstructions = '''The user will provide a phrase, and  check if it is a command or not. If it is a supported command response format:{}.
         If it is not a supported command,response format:{}.
         The supported commands={}  inputs required={}.
         Dont add Response or AI in the staring of response
                 '''
-                
-                inst1 = '''{"positive":<response if task can be done or is  executed correctly>,"negative":<response if  not executed>, "command_name": <camel case name of the command>,
+
+        inst1 = '''{"positive":<response if task can be done or is  executed correctly>,"negative":<response if  not executed>, "command_name": <camel case name of the command>,
         "inputs":<inputs that the command might require from the phrase>}'''
 
-                inst2 = '''{"response":"Not a valid command","command_name:"NotACommand"}'''
+        inst2 = '''{"response":"Not a valid command","command_name:"NotACommand"}'''
 
-                self.aiStory = '''
+        self.aiStory = '''
                 The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and sarcastic.Every response ends with movement control ,and an emotion command sepearted by & (movement:forward,backward,right,left,emotion:happy,sad,angry,serious,laugh,think), the bot uses this to express it's emotions For example(Thank you&right&happy,alaram set&forward,backward&happy).The assistant was created by Avanish,Monish,Ramesh and Dhruva. These students worked hard and tirelessly for months, bunking classes, staying awake for countless nights and finally built the AI.
         Human: Hello, who are you?
         AI: I am an AI created by OpenAI. How can I help you today?&right&happy
@@ -105,46 +104,44 @@ class AI:
         AI:I'm sorry to hear that. Is there anything I can do to help?&left&serious
         Human:
                 '''
-                # self.file=open("out.txt","w")
-                self.pluginsObject = []
-                self.inputParams = {}
-                self.commandName = []
-                if len(pluginArray) != 0:
-                    self.setUpPlugin(pluginArray)
-                self.aiInstructions = self.aiInstructions.format(
-                    inst1, inst2, self.commandName, self.inputParams)
+        # self.file=open("out.txt","w")
+        self.pluginsObject = []
+        self.inputParams = {}
+        self.commandName = []
+        if len(pluginArray) != 0:
+            self.setUpPlugin(pluginArray)
+        self.aiInstructions = self.aiInstructions.format(
+            inst1, inst2, self.commandName, self.inputParams)
 
-                self.mic = pyaudio.PyAudio()
-                self.stream = self.mic.open(
-                    format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8192)
+        self.mic = pyaudio.PyAudio()
+        self.stream = self.mic.open(
+            format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8192)
 
-                self.engine = tts.init()
-                self.engine.setProperty('rate', 160)
-                self.engine.setProperty('volume', 0.9)
-                self.engine.setProperty('voice', 'english_rp+f4')
-                
-                self.motorControl=None
-                self.motorDuration=2
+        self.engine = tts.init()
+        self.engine.setProperty('rate', 160)
+        self.engine.setProperty('volume', 0.9)
+        self.engine.setProperty('voice', 'english_rp+f4')
 
-                # self.test()
-    def startDisplay(self,name):
+        self.motorControl = None
+        self.motorDuration = 2
+
+        # self.test()
+    def startDisplay(self, name):
         self.stopEvent.clear()
-        self.x=threading.Thread(target=self.DisplayImage,args=(name,))
+        self.x = threading.Thread(target=self.DisplayImage, args=(name,))
         self.x.setDaemon(True)
         self.x.start()
 
-    def DisplayImage(self,name):
+    def DisplayImage(self, name):
         while not self.stopEvent.is_set():
-            self.oled.drawSavedImage(name) 
+            self.oled.drawSavedImage(name)
         return
-    
 
-    def changeImage(self,name):
-        print("changing image to",name)
+    def changeImage(self, name):
+        print("changing image to", name)
         self.stopEvent.set()
         self.x.join()
         self.startDisplay(name)
-
 
     def test(self):
         self.main("hello")
@@ -153,21 +150,21 @@ class AI:
             self.test()
         pass
 
-    def main(self,prompt=""):
-        
-        openai.api_key=self.api_key_1
+    def main(self, prompt=""):
+
+        openai.api_key = self.api_key_1
         try:
-            if prompt=="":
+            if prompt == "":
                 prompt = self.SpeechTotextOffline()
                 self.changeImage("processing")
                 print("we entered")
-                
+
             print(prompt)
             self.generateCommandResponse(prompt)
 
             return True
         except Exception as e:
-            print("Error ",e)
+            print("Error ", e)
 
             return False
 
@@ -178,34 +175,33 @@ class AI:
         for plugin in pluginArray:
             self.pluginsObject.append(plugin)
             self.commandName.append(plugin.commandName)
-            self.inputParams[plugin.commandName]=plugin.inputFormat
+            self.inputParams[plugin.commandName] = plugin.inputFormat
 
         # self.aiInstructions=self.aiInstructions.format(self.commandName,self.inputParams)
-    
 
-    def setupMotionControl(self,motorObject):
-        self.motionControl=motorObject
+    def setupMotionControl(self, motorObject):
+        self.motionControl = motorObject
 
-
-    def movement(self,what):
-        if self.motorControl==None:
+    def movement(self, what):
+        if self.motorControl == None:
             print("Bot MotorControl is Absent")
             return
 
-        thread=threading.Thread(self.motorControl.AIContorl,args=(what,self.motorDuration))
+        thread = threading.Thread(
+            self.motorControl.AIContorl, args=(what, self.motorDuration))
         thread.start()
         thread.join()
 
     def textToSpeech(self, text):
         if self.engine._inLoop:
             self.engine.endLoop()
-        
+
         self.engine.say(text)
         self.engine.runAndWait()
 
-    def generateNormalResponse(self,prompt):
-        openai.api_key=self.api_key_2
-        Modprompt=self.aiStory+prompt+'\n'+'AI:'
+    def generateNormalResponse(self, prompt):
+        openai.api_key = self.api_key_2
+        Modprompt = self.aiStory+prompt+'\n'+'AI:'
         response = openai.Completion.create(
             model="text-davinci-003",
             prompt=Modprompt,
@@ -216,26 +212,26 @@ class AI:
             presence_penalty=0
         )
         response = response["choices"][0]["text"].split("&")
-        print("Response1=",response)
+        print("Response1=", response)
         try:
             self.changeImage(response[-1])
         except:
             print("error")
-        
-        what=''
+
+        what = ''
         try:
-            what=response[1]
+            what = response[1]
 
         except:
-            what=None
+            what = None
 
         self.movement(what)
         self.textToSpeech(response[0])
-        
+
     def generateCommandResponse(self, prompt):
 
         Modprompt = self.aiInstructions+'\n'+prompt
-        print("Mod prompt=",Modprompt)
+        print("Mod prompt=", Modprompt)
         response = openai.Completion.create(
             model="text-davinci-003",
             prompt=Modprompt,
@@ -246,10 +242,10 @@ class AI:
             presence_penalty=0
         )
         response = response["choices"][0]["text"]
-        print("Response=",response)
-        self.interpretTheResponse(response,prompt)
+        print("Response=", response)
+        self.interpretTheResponse(response, prompt)
 
-    def interpretTheResponse(self, aiResponse,prompt):
+    def interpretTheResponse(self, aiResponse, prompt):
         responseObject = eval(aiResponse)
         # self.file.write('\n'+responseObject)
         commandName = responseObject['command_name']
@@ -278,12 +274,12 @@ class AI:
         tempStream = self.stream
         tempStream.start_stream()
         while True:
-            data = tempStream.read(4096,exception_on_overflow=0)
+            data = tempStream.read(4096, exception_on_overflow=0)
             if recognizer.AcceptWaveform(data):
                 text = recognizer.Result()
                 text = text[14:-3]
                 print(text)
-                if len(text)!=0:
+                if len(text) != 0:
                     return text
 
 
--- motor.py
+++ motor.py
@@ -1,205 +1,200 @@
-import RPi.GPIO as GPIO          
+import RPi.GPIO as GPIO
 from time import sleep
 
 
-
 class Motor:
-    en1=25
-    en2=22
+    en1 = 25
+    en2 = 22
 
-    in1=23
-    in2=24
+    in1 = 23
+    in2 = 24
 
-    in3=17
-    in4=27
-    def __init__(self,setup=False):
+    in3 = 17
+    in4 = 27
+
+    def __init__(self, setup=False):
         if setup:
             self.setup()
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.in1,GPIO.OUT)
-        GPIO.setup(self.in2,GPIO.OUT)
-        GPIO.setup(self.en1,GPIO.OUT)
-        GPIO.output(self.in1,GPIO.LOW)
-        GPIO.output(self.in2,GPIO.LOW)
-        self.p1=GPIO.PWM(self.en1,1000)
+        GPIO.setup(self.in1, GPIO.OUT)
+        GPIO.setup(self.in2, GPIO.OUT)
+        GPIO.setup(self.en1, GPIO.OUT)
+        GPIO.output(self.in1, GPIO.LOW)
+        GPIO.output(self.in2, GPIO.LOW)
+        self.p1 = GPIO.PWM(self.en1, 1000)
         self.p1.start(50)
 
-
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.in3,GPIO.OUT)
-        GPIO.setup(self.in4,GPIO.OUT)
-        GPIO.setup(self.en2,GPIO.OUT)
-        GPIO.output(self.in3,GPIO.LOW)
-        GPIO.output(self.in4,GPIO.LOW)
-        self.p2=GPIO.PWM(self.en2,1000)
+        GPIO.setup(self.in3, GPIO.OUT)
+        GPIO.setup(self.in4, GPIO.OUT)
+        GPIO.setup(self.en2, GPIO.OUT)
+        GPIO.output(self.in3, GPIO.LOW)
+        GPIO.output(self.in4, GPIO.LOW)
+        self.p2 = GPIO.PWM(self.en2, 1000)
         self.p2.start(50)
 
     def setup(self):
-        self.en1=int(input("Enter Enable 1 :"))
-        self.en2=int(input("Enter Enable 2 :"))
+        self.en1 = int(input("Enter Enable 1 :"))
+        self.en2 = int(input("Enter Enable 2 :"))
 
-        self.in1=int(input("Enter Input 1 :"))
-        self.in2=int(input("Enter Input 2 :"))
+        self.in1 = int(input("Enter Input 1 :"))
+        self.in2 = int(input("Enter Input 2 :"))
 
-        self.in3=int(input("Enter Input 3 :"))
-        self.in4=int(input("Enter Input 4 :"))
+        self.in3 = int(input("Enter Input 3 :"))
+        self.in4 = int(input("Enter Input 4 :"))
 
-    def userControl(self,what):
-        GPIO.output(self.in1,GPIO.LOW)
-        GPIO.output(self.in2,GPIO.LOW)
+    def userControl(self, what):
+        GPIO.output(self.in1, GPIO.LOW)
+        GPIO.output(self.in2, GPIO.LOW)
 
-        GPIO.output(self.in3,GPIO.LOW)
-        GPIO.output(self.in4,GPIO.LOW)
-        if what=='up':
-            GPIO.output(self.in1,GPIO.HIGH)
-            GPIO.output(self.in2,GPIO.LOW)
+        GPIO.output(self.in3, GPIO.LOW)
+        GPIO.output(self.in4, GPIO.LOW)
+        if what == 'up':
+            GPIO.output(self.in1, GPIO.HIGH)
+            GPIO.output(self.in2, GPIO.LOW)
 
-            GPIO.output(self.in3,GPIO.HIGH)
-            GPIO.output(self.in4,GPIO.LOW)
+            GPIO.output(self.in3, GPIO.HIGH)
+            GPIO.output(self.in4, GPIO.LOW)
 
-        elif what=='down':
-            GPIO.output(self.in1,GPIO.LOW)
-            GPIO.output(self.in2,GPIO.HIGH)
+        elif what == 'down':
+            GPIO.output(self.in1, GPIO.LOW)
+            GPIO.output(self.in2, GPIO.HIGH)
 
-            GPIO.output(self.in3,GPIO.LOW)
-            GPIO.output(self.in4,GPIO.HIGH)
-        elif what=='left':
-            GPIO.output(self.in1,GPIO.LOW)
-            GPIO.output(self.in2,GPIO.HIGH)
+            GPIO.output(self.in3, GPIO.LOW)
+            GPIO.output(self.in4, GPIO.HIGH)
+        elif what == 'left':
+            GPIO.output(self.in1, GPIO.LOW)
+            GPIO.output(self.in2, GPIO.HIGH)
 
-            GPIO.output(self.in3,GPIO.HIGH)
-            GPIO.output(self.in4,GPIO.LOW)
-        elif what=='right':
-            GPIO.output(self.in1,GPIO.HIGH)
-            GPIO.output(self.in2,GPIO.LOW)
+            GPIO.output(self.in3, GPIO.HIGH)
+            GPIO.output(self.in4, GPIO.LOW)
+        elif what == 'right':
+            GPIO.output(self.in1, GPIO.HIGH)
+            GPIO.output(self.in2, GPIO.LOW)
 
-            GPIO.output(self.in3,GPIO.LOW)
-            GPIO.output(self.in4,GPIO.HIGH)
+            GPIO.output(self.in3, GPIO.LOW)
+            GPIO.output(self.in4, GPIO.HIGH)
         else:
-            GPIO.output(self.in1,GPIO.LOW)
-            GPIO.output(self.in2,GPIO.LOW)
-            GPIO.output(self.in3,GPIO.LOW)
-            GPIO.output(self.in4,GPIO.LOW)
+            GPIO.output(self.in1, GPIO.LOW)
+            GPIO.output(self.in2, GPIO.LOW)
+            GPIO.output(self.in3, GPIO.LOW)
+            GPIO.output(self.in4, GPIO.LOW)
 
-    def AIControl(self,what,duration):
-        if what=='up':
+    def AIControl(self, what, duration):
+        if what == 'up':
             self.forward(duration)
-        elif what=='down':
+        elif what == 'down':
             self.backward(duration)
-        elif what=='left':
+        elif what == 'left':
             self.left(duration)
-        elif what=='right':
+        elif what == 'right':
             self.right(duration)
         else:
             self.threeSixty()
 
     def stop(self):
-        GPIO.output(self.in1,GPIO.LOW)
-        GPIO.output(self.in2,GPIO.LOW)
-        GPIO.output(self.in3,GPIO.LOW)
-        GPIO.output(self.in4,GPIO.LOW)
+        GPIO.output(self.in1, GPIO.LOW)
+        GPIO.output(self.in2, GPIO.LOW)
+        GPIO.output(self.in3, GPIO.LOW)
+        GPIO.output(self.in4, GPIO.LOW)
 
+    def forward(self, time):
 
-    def forward(self,time):
+        GPIO.output(self.in1, GPIO.HIGH)
+        GPIO.output(self.in2, GPIO.LOW)
 
-        GPIO.output(self.in1,GPIO.HIGH)
-        GPIO.output(self.in2,GPIO.LOW)
-
-        GPIO.output(self.in3,GPIO.HIGH)
-        GPIO.output(self.in4,GPIO.LOW)
+        GPIO.output(self.in3, GPIO.HIGH)
+        GPIO.output(self.in4, GPIO.LOW)
 
         sleep(time)
 
-        GPIO.output(self.in1,GPIO.LOW)
-        GPIO.output(self.in2,GPIO.LOW)
+        GPIO.output(self.in1, GPIO.LOW)
+        GPIO.output(self.in2, GPIO.LOW)
 
-        GPIO.output(self.in3,GPIO.LOW)
-        GPIO.output(self.in4,GPIO.LOW)
+        GPIO.output(self.in3, GPIO.LOW)
+        GPIO.output(self.in4, GPIO.LOW)
 
         pass
-    
-    def backward(self,time):
-        GPIO.output(self.in1,GPIO.LOW)
-        GPIO.output(self.in2,GPIO.HIGH)
 
-        GPIO.output(self.in3,GPIO.LOW)
-        GPIO.output(self.in4,GPIO.HIGH)
+    def backward(self, time):
+        GPIO.output(self.in1, GPIO.LOW)
+        GPIO.output(self.in2, GPIO.HIGH)
 
-        sleep(time)
-        
-        GPIO.output(self.in1,GPIO.LOW)
-        GPIO.output(self.in2,GPIO.LOW)
-
-        GPIO.output(self.in3,GPIO.LOW)
-        GPIO.output(self.in4,GPIO.LOW)
-
-    def right(self,time):
-        GPIO.output(self.in1,GPIO.HIGH)
-        GPIO.output(self.in2,GPIO.LOW)
-
-        GPIO.output(self.in3,GPIO.LOW)
-        GPIO.output(self.in4,GPIO.HIGH)
+        GPIO.output(self.in3, GPIO.LOW)
+        GPIO.output(self.in4, GPIO.HIGH)
 
         sleep(time)
-        
-        GPIO.output(self.in1,GPIO.LOW)
-        GPIO.output(self.in2,GPIO.LOW)
 
-        GPIO.output(self.in3,GPIO.LOW)
-        GPIO.output(self.in4,GPIO.LOW)
+        GPIO.output(self.in1, GPIO.LOW)
+        GPIO.output(self.in2, GPIO.LOW)
 
-    def left(self,time):
-        GPIO.output(self.in1,GPIO.LOW)
-        GPIO.output(self.in2,GPIO.HIGH)
+        GPIO.output(self.in3, GPIO.LOW)
+        GPIO.output(self.in4, GPIO.LOW)
 
-        GPIO.output(self.in3,GPIO.HIGH)
-        GPIO.output(self.in4,GPIO.LOW)
-        
+    def right(self, time):
+        GPIO.output(self.in1, GPIO.HIGH)
+        GPIO.output(self.in2, GPIO.LOW)
+
+        GPIO.output(self.in3, GPIO.LOW)
+        GPIO.output(self.in4, GPIO.HIGH)
 
         sleep(time)
-        
-        GPIO.output(self.in1,GPIO.LOW)
-        GPIO.output(self.in2,GPIO.LOW)
 
-        GPIO.output(self.in3,GPIO.LOW)
-        GPIO.output(self.in4,GPIO.LOW)
+        GPIO.output(self.in1, GPIO.LOW)
+        GPIO.output(self.in2, GPIO.LOW)
+
+        GPIO.output(self.in3, GPIO.LOW)
+        GPIO.output(self.in4, GPIO.LOW)
+
+    def left(self, time):
+        GPIO.output(self.in1, GPIO.LOW)
+        GPIO.output(self.in2, GPIO.HIGH)
+
+        GPIO.output(self.in3, GPIO.HIGH)
+        GPIO.output(self.in4, GPIO.LOW)
+
+        sleep(time)
+
+        GPIO.output(self.in1, GPIO.LOW)
+        GPIO.output(self.in2, GPIO.LOW)
+
+        GPIO.output(self.in3, GPIO.LOW)
+        GPIO.output(self.in4, GPIO.LOW)
 
     def threeSixty(self):
-        GPIO.output(self.in1,GPIO.HIGH)
-        GPIO.output(self.in2,GPIO.LOW)
+        GPIO.output(self.in1, GPIO.HIGH)
+        GPIO.output(self.in2, GPIO.LOW)
 
-        GPIO.output(self.in3,GPIO.LOW)
-        GPIO.output(self.in4,GPIO.HIGH)
-
-        sleep(5)
-        
-        GPIO.output(self.in1,GPIO.LOW)
-        GPIO.output(self.in2,GPIO.LOW)
-
-        GPIO.output(self.in3,GPIO.LOW)
-        GPIO.output(self.in4,GPIO.LOW)
-
-        GPIO.output(self.in1,GPIO.LOW)
-        GPIO.output(self.in2,GPIO.HIGH)
-
-        GPIO.output(self.in3,GPIO.HIGH)
-        GPIO.output(self.in4,GPIO.LOW)
-        
+        GPIO.output(self.in3, GPIO.LOW)
+        GPIO.output(self.in4, GPIO.HIGH)
 
         sleep(5)
-        
-        GPIO.output(self.in1,GPIO.LOW)
-        GPIO.output(self.in2,GPIO.LOW)
 
-        GPIO.output(self.in3,GPIO.LOW)
-        GPIO.output(self.in4,GPIO.LOW)
+        GPIO.output(self.in1, GPIO.LOW)
+        GPIO.output(self.in2, GPIO.LOW)
+
+        GPIO.output(self.in3, GPIO.LOW)
+        GPIO.output(self.in4, GPIO.LOW)
+
+        GPIO.output(self.in1, GPIO.LOW)
+        GPIO.output(self.in2, GPIO.HIGH)
+
+        GPIO.output(self.in3, GPIO.HIGH)
+        GPIO.output(self.in4, GPIO.LOW)
+
+        sleep(5)
+
+        GPIO.output(self.in1, GPIO.LOW)
+        GPIO.output(self.in2, GPIO.LOW)
+
+        GPIO.output(self.in3, GPIO.LOW)
+        GPIO.output(self.in4, GPIO.LOW)
         pass
 
-
-    def power(self,power):
+    def power(self, power):
         self.p1.ChangeDutyCycle(power)
         self.p2.ChangeDutyCycle(power)
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     Motor()
